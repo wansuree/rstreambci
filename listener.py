a99521527b0eba52c04ce8c9f9fb7a01496b63dd
Trevor Alexander <talex@privatdemail.net>
2014-06-12 00:34:36 -1000

    listener port changed to 5005
    default bci2000 UDP port

--- listener.py
+++ listener.py
@@ -1,7 +1,7 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5000 # bci2000 sends over port 5000 by default
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
