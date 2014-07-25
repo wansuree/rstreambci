d673deffac2cb1c7de9213d5a25877129460b10f
Trevor Alexander <talex@privatdemail.net>
2014-07-25 13:46:08 -1000

    Added first parsing code.
    Also added sample output snippet in case BCI2000 is not available.

--- listener.py
+++ listener.py
@@ -1,4 +1,5 @@
 import socket
+from array import array
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5000 # bci2000 sends over port 5000 by default
@@ -7,6 +8,65 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+# Things we get from the AppConnector (incomplete list in order of importance):
+# StimulusTime -- time in ms?
+# Signal(x,y) -- voltage in microvolts? what are x and y?
+# SelectedTarget -- should be class label
+# SelectedRow -- selected row of speller
+# SelectedColumn -- selected column of speller
+# Running -- Whether or not test is running?
+# Recording -- Umm?
+# ...delimited by empty message and LF..
+
+stimtime = array('I') # unsigned int
+sigvolt = array('d') # double
+target = array('c') # char (should be binary, basically)
+rowsel = array('I') # unsigned int
+colsel = array('I') # unsigned int
+isrun = array('c') # char (binary)
+isrec = array('c') # char (binary)
+
+
+
+# dictionary for label parsing
+def ltime():
+	stimtime.append(parsedata[1])
+	
+def lsig(): # TODO doesn't work yet, we need a more clever test in the dict
+	sigvolt.append(parsedata[1])
+
+def ltarg():
+	target.append(parsedata[1])
+	
+def lrow():
+	rowsel.append(parsedata[1])
+	
+def lcol():
+	colsel.append(parsedata[1])
+	
+def lrun():
+	isrun.append(parsedata[1])
+
+def lrec():
+	isrec.append(parsedata[1])
+
+parsing = {"StimulusTime" : ltime,
+		  "Signal" : lsig,
+		  "SelectedTarget" : ltarg,
+		  "SelectedRow" : lrow,
+		  "SelectedColumn" : lcol,
+		  "Running" : lrun,
+		  "Recording" : lrec,
+}
+
+
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    
+	print data
+	# split the data into label and value
+	parsedata = data.split()
+
+	# actually parse
+	# TODO needs error handling
+	parsing[parsedata[0]]()
