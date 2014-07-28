f13c235aec7e90728cfd6b4fadccc7ce12fc27b2
Trevor Alexander <talex@privatdemail.net>
2014-07-27 21:26:11 -1000

    Modified test sender to accept text from stdin.

--- testsender.py
+++ testsender.py
@@ -1,13 +1,23 @@
 import socket
+import sys
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5000
 MESSAGE = "Hello, World!"
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+# print "message:", MESSAGE
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+# Send from stdin
+while True:
+  line = sys.stdin.readline()
+  if line:
+    sock.sendto(line, (UDP_IP, UDP_PORT))
+  else:
+    break
+
+sock.close()
